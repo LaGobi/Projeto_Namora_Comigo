d581c08e67a8b2a837ccf8419caf43b1704b03bf
Laura Gobi <lauragoby2016@gmail.com>
2024-08-21 22:53:47 -0300

    Arquivos novos

--- scrip.js
+++ scrip.js
@@ -1,15 +1,17 @@
-function sim(){
-    alert("Você aceitou namorar comigo!  :)")
-}
+        function sim(){
+            
+            alert("Você aceitou namorar comigo! :)")
+        }
 
-function desvia(t){
-var btn = t
-btn.style.position = 'absolute';
-btn.style.bottom = geraPosicao (10, 90);
-btn.style.left = geraPosicao (10, 90);
-console.log("opa, desviei...")
-}
+        function desvia(t){
+            var btn = t;
+            btn.style.position = 'absolute';
+            btn.style.bottom = geraPosicao(10, 70);
+            btn.style.left = geraPosicao(10, 70)
+            console.log("opa, desviei...");
+        }
 
-function geraPosicao (min, max) {
-    return Math.random() * (max - min) + "%";
-}
+        function geraPosicao(min, max) {
+            return (Math.random() * (max - min) + min) + "%";
+        }
+
